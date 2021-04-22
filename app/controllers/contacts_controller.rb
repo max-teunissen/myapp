ac8df99a398a09248a33b966b62d509ceb8b5837
Max Teunissen <max.teunissen@mac.com>
2021-04-22 16:35:45 +0200

    a

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,8 +8,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:error] = nil
-      redirect_to root_path, flash.now[:notice] = 'Message sent successfully'
+      flash.now[:notive] = 'Message sent successfully'
     else
       flash.now[:error] = 'Cannot send message'
       render :new
